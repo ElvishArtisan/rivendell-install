c550fa832e8d8d27343816f5cabb5e7c1aa9599f
Fred Gleason <fredg@paraelsystems.com>
2016-07-08 21:29:28 -0400

    2016-07-08 Fred Gleason <fredg@paravelsystems.com> 	* Tweaked install on RHEL 7.

--- centos7/install_rivendell.sh
+++ centos7/install_rivendell.sh
@@ -35,25 +35,30 @@ systemctl enable mariadb
 #
 patch /etc/selinux/config /usr/share/rivendell-install/disable-selinux.patch
 systemctl disable firewalld
+rm -f /etc/asound.conf
 cp /usr/share/rivendell-install/asound.conf /etc/
 cp /usr/share/rivendell-install/*.repo /etc/yum.repos.d/
 cp /usr/share/rivendell-install/RPM-GPG-KEY* /etc/pki/rpm-gpg/
 mkdir -p /usr/share/pixmaps/rivendell
 cp /usr/share/rivendell-install/rdairplay_skin.png /usr/share/pixmaps/rivendell/
 cp /usr/share/rivendell-install/rdpanel_skin.png /usr/share/pixmaps/rivendell/
+mv /etc/samba/smb.conf /etc/samba/smb-original.conf
 cp /usr/share/rivendell-install/smb.conf /etc/samba/
 cp /usr/share/rivendell-install/no_screen_blank.conf /etc/X11/xorg.conf.d/
 mkdir -p /etc/skel/Desktop
 cp /usr/share/rivendell-install/skel/rog-1.3.3.pdf /etc/skel/Desktop/Rivendell\ Ops\ Guide.pdf
-mkdir -p /etc/skel/rd_xfer
-mkdir -p /etc/skel/music_export
-mkdir -p /etc/skel/music_import
-mkdir -p /etc/skel/traffic_export
-mkdir -p /etc/skel/traffic_import
+cp /usr/share/rivendell-install/skel/paravel_support.pdf /etc/skel/Desktop/First\ Steps.pdf
 mkdir -p /etc/skel/.qt
 cp /usr/share/rivendell-install/qt_plugins_3.3rc /etc/skel/.qt/
 cp /usr/share/rivendell-install/qtrc /etc/skel/.qt/
 adduser -c Rivendell\ Audio --groups audio rd
+mkdir -p /home/rd/rd_xfer
+mkdir -p /home/rd/music_export
+mkdir -p /home/rd/music_import
+mkdir -p /home/rd/traffic_export
+mkdir -p /home/rd/traffic_import
+chown -R rd:rd /home/rd
+chmod 0755 /home/rd
 patch /etc/gdm/custom.conf /usr/share/rivendell-install/autologin.patch
 yum -y remove alsa-firmware alsa-firmware-tools
 yum -y install rivendell
