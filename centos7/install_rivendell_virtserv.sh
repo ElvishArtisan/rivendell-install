facd1c01cbd0cf4fa284e4052d395e5948dbc396
Fred Gleason <fredg@paravelsystems.com>
2020-03-20 00:38:44 -0400

    2020-03-20 Fred Gleason <fredg@paravelsystems.com> 	* Added 'centos7/rivendell-install-virtserv.sh'.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

---
+++ centos7/install_rivendell_virtserv.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# install_rivendell_virtserv.sh
+#
+# Install a KVM hypervisor setup suitable for running Rivendell vhosts
+#
+#    Copyright (C) 2020 Fred Gleason <fredg@paravelsystems.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of version 2 of the GNU General Public License as
+#    published by the Free Software Foundation;
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, 
+#    Boston, MA  02111-1307  USA
+#
+
+#
+# Install Packages
+#
+yum -y install qemu-kvm libvirt libvirt-python libguestfs-tools virt-install virt-manager
+
+#
+# Enable and start the hypervisor
+#
+systemctl enable libvirtd
+systemctl start libvirtd
+
+#
+# Finish Up
+#
+echo
+echo "Installation of the Rivendell development tools is complete."
+echo
